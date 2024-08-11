4d7ad39367e85b3ba6422228010d647608491f4f
jubayer <jubayerahmed46@gmail.com>
2024-08-11 18:46:55 +0600

    adde trending games section and responsive for all the devices

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// Loader
+window.onload = () => {
+    document.querySelector('.loader').style.display = 'none';
+}
 const searchBtn = document.querySelector('.search-btn');
 const menuBar = document.querySelector('.bar ion-icon');
 const closeBtn = document.querySelector('.close-btn');
@@ -18,7 +22,65 @@ function showHideNavBar() {
 searchBtn.addEventListener('click', showHideNavBar);
 menuBar.addEventListener('click', showHideNavBar);
 closeBtn.addEventListener('click', showHideNavBar);
-// loader
-window.onload = () => {
-    document.querySelector('.loader').style.display = 'none';
-}
+
+// trending game section deopdown fucntionalily
+const gamerNavs = document.querySelector('.navs');
+
+const gmsLzBtns = document.querySelectorAll('.navigator li');
+const gmsSmBtns = document.querySelectorAll('.small-navigator option');
+
+const wonderland = document.querySelector('#wonderland');
+const wraith = document.querySelector('#wraith');
+const apexLegends = document.querySelector('#apex-legends');
+const bunnyOfficer = document.querySelector('#bunny-officer');
+
+gamerNavs.addEventListener('click', function(event) {
+    let value = event.target;
+       if(value) {
+            gmsLzBtns.forEach((btn, i) => {
+                btn.style.color = '#fff';
+                gmsSmBtns[i].style.color = '#fff';
+                value.style.color = '#ffc010';
+            }) 
+       }
+
+    switch(value.innerText.toLocaleLowerCase()) {
+        case 'all':
+            wonderland.style.display = 'block';
+            wraith.style.display = 'block';
+            apexLegends.style.display = 'block';
+            bunnyOfficer.style.display = 'block';
+        break
+        case 'origin':
+            wonderland.style.display = 'block';
+            wraith.style.display = 'none';
+            apexLegends.style.display = 'none';
+            bunnyOfficer.style.display = 'block';
+        break
+        case 'playstation 4':
+            wonderland.style.display = 'block';
+            wraith.style.display = 'none';
+            apexLegends.style.display = 'none';
+            bunnyOfficer.style.display = 'none';
+        break
+        case 'uplay':
+            wonderland.style.display = 'none';
+            wraith.style.display = 'none';
+            apexLegends.style.display = 'block';
+            bunnyOfficer.style.display = 'none';
+        break
+        case 'steam':
+            wonderland.style.display = 'none';
+            wraith.style.display = 'block';
+            apexLegends.style.display = 'none';
+            bunnyOfficer.style.display = 'none';
+
+        break
+        case 'reper':
+            wonderland.style.display = 'none';
+            wraith.style.display = 'none';
+            apexLegends.style.display = 'none';
+            bunnyOfficer.style.display = 'none';
+        break
+    }
+})
